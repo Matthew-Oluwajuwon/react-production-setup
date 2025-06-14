21015549892ed936882cd2e10ce2ccdf6cf7711d
Matarazy <matthewoluwajuwon056@gmail.com>
2025-06-14 19:29:37 +0100

    feat: updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import { Layout } from '@/shared'
+import * as Sentry from '@sentry/react'
 
 function App() {
-    return <Layout>Hello world!!!</Layout>
+    const handleClick = () => {
+        try {
+            throw new Error('Sentry test error from UI button')
+        } catch (error) {
+            Sentry.captureException(error)
+        }
+    }
+
+    return (
+        <Layout>
+            Hello world!!!
+            <button onClick={handleClick}>Hello</button>
+        </Layout>
+    )
 }
 export default App
