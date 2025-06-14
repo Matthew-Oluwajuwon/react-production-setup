f7af2d4236696c82c53ba7aa9ffa7c32449c2e24
Matarazy <matthewoluwajuwon056@gmail.com>
2025-06-14 19:32:54 +0100

    feat: updated App.tsx

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,10 +1,26 @@
-import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { captureException } from '@sentry/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { describe, expect, it, vi } from 'vitest'
 import App from '../App'
+
+// Mock @sentry/react
+vi.mock('@sentry/react', () => ({
+    captureException: vi.fn()
+}))
 
 describe('App', () => {
     it('renders Layout component with content', () => {
         render(<App />)
         expect(screen.getByText('Hello world!!!')).toBeInTheDocument()
+    })
+
+    it('calls Sentry.captureException on button click', () => {
+        render(<App />)
+
+        const button = screen.getByRole('button', { name: /hello/i })
+        fireEvent.click(button)
+
+        expect(captureException).toHaveBeenCalledTimes(1)
+        expect(captureException.mock.calls[0][0].message).toBe('Sentry test error from UI button')
     })
 })
