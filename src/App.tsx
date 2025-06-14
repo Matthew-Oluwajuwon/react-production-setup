c370c57b49aa5ec576dc5bc40194b4fbcafa707c
Matarazy <matthewoluwajuwon056@gmail.com>
2025-06-14 13:44:13 +0100

    feat: updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Layout } from '@/shared'
 
 function App() {
-    return <Layout>Hello world!!!</Layout>
+    const isTrue = true
+    return <Layout>Hello world!!!{isTrue || 'False'}</Layout>
 }
 export default App
