6509ef3cf5c8320e012653004e5609ae554f6476
Matarazy <matthewoluwajuwon056@gmail.com>
2025-06-14 01:22:49 +0100

    feat: sonarqube setup

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ const validateEnv = (envMode: EnvMode, env: AppEnv) => {
 
     for (const key of requiredVars) {
         if (!env[key]) {
-            throw new Error(`Missing environment variable!: ${key} in mode ${envMode}`)
+            throw new Error(`Missing environment variable: ${key} in mode ${envMode}`)
         }
     }
 }
@@ -32,7 +32,7 @@ const validateEnv = (envMode: EnvMode, env: AppEnv) => {
 const normalizePort = (port: string): number => {
     const parsedPort = parseInt(port, 10)
     if (isNaN(parsedPort)) {
-        throw new Error(`Invalid port number!: ${port}`)
+        throw new Error(`Invalid port number: ${port}`)
     }
     return parsedPort >= 0 ? parsedPort : 3000 // Ensure port is non-negative
 }
